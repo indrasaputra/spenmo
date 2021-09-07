2fa3ea88e38b078f9bea3265e074a2d256fba5ca
Indra Saputra <flame.fire99@gmail.com>
2021-09-07 15:04:49 +0700

    feat: implement builder for project

---
+++ internal/builder/spenmo.go
@@ -0,0 +1,35 @@
+package builder
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/indrasaputra/spenmo/internal/config"
+	"github.com/indrasaputra/spenmo/internal/grpc/handler"
+	"github.com/indrasaputra/spenmo/internal/repository/postgres"
+	"github.com/indrasaputra/spenmo/service"
+)
+
+// BuildCardCommandHandler builds card command handler including all of its dependencies.
+func BuildCardCommandHandler(pool *pgxpool.Pool) *handler.CardCommand {
+	psql := postgres.NewCard(pool)
+	creator := service.NewCardCreator(psql)
+	return handler.NewCardCommand(creator)
+}
+
+// BuildPgxPool builds a pool of pgx client.
+func BuildPgxPool(cfg *config.Postgres) (*pgxpool.Pool, error) {
+	connCfg := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable pool_max_conns=%s pool_max_conn_lifetime=%s pool_max_conn_idle_time=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.User,
+		cfg.Password,
+		cfg.Name,
+		cfg.MaxOpenConns,
+		cfg.MaxConnLifetime,
+		cfg.MaxIdleLifetime,
+	)
+	return pgxpool.Connect(context.Background(), connCfg)
+}
---
+++ internal/builder/spenmo_test.go
@@ -0,0 +1,39 @@
+package builder_test
+
+import (
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/indrasaputra/spenmo/internal/builder"
+	"github.com/indrasaputra/spenmo/internal/config"
+)
+
+func TestBuildCardCommandHandler(t *testing.T) {
+	t.Run("success create card command handler", func(t *testing.T) {
+		psql := &pgxpool.Pool{}
+		handler := builder.BuildCardCommandHandler(psql)
+		assert.NotNil(t, handler)
+	})
+}
+
+func TestBuildPgxPool(t *testing.T) {
+	cfg := &config.Postgres{
+		Host:            "localhost",
+		Port:            "5432",
+		Name:            "spenmo",
+		User:            "user",
+		Password:        "password",
+		MaxOpenConns:    "10",
+		MaxConnLifetime: "10m",
+		MaxIdleLifetime: "5m",
+	}
+
+	t.Run("fail build sql client", func(t *testing.T) {
+		client, err := builder.BuildPgxPool(cfg)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, client)
+	})
+}
